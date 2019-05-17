c24083ac6194f56fa2efc5148a31df0000c9fa83
Julien DECOTTIGNIES <jdecottignies@absysycborg.com>
2019-05-17 16:57:40 +0200

    add routes

---
+++ src/database.ts
@@ -0,0 +1,33 @@
+import * as sql from "mssql";
+import dbconfig from "../dbconfig.json";
+import express = require('express');
+
+const dbConfig: sql.config = dbconfig;
+
+export function executeQuery(response: express.Response, query: string) {
+
+    let connection: sql.ConnectionPool = new sql.ConnectionPool(dbConfig, (err: any) => {
+        if (err) {
+            console.log(`An error occured: ${err}`);
+            response.status(400).send(err);
+        }
+
+
+        else {
+            // Create a request
+            let sqlRequest = new sql.Request(connection);
+
+            // query database
+
+            sqlRequest.query(query, (err: any, sqlResponse: any) => {
+                if (err) {
+                    console.log(`Error while querying the database: ${err}`);
+                    response.status(400).send(err);
+                }
+                else {
+                    response.status(200).send(sqlResponse);
+                }
+            });
+        }
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,103 +1,78 @@
-import * as sql from "mssql";
 import * as bodyParser from "body-parser";
+import * as db from './database';
 import express = require('express');
 
 const app: express.Application = express();
-let router = express.Router();
+var router = express.Router();
 
-// app.set('case sensitive routing', true);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+router.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log('In Router');
 
-const server = app.listen(3000, () => {
-        console.log(`running on port 3000`);
+    next();
 });
 
-const dbConfig: sql.config = {
-    user: 'julien',
-    password: 'Dks2019$',
-    server: 'localhost',
-    options: { 
-        instanceName: 'SQLEXPRESS' 
-    },
-    database: 'nodeconnect'
-}
-
-
-
-
-
-var executeQuery = (response: express.Response, query: string) => {
-
-    let connection: sql.ConnectionPool = new sql.ConnectionPool(dbConfig, (err: any) => {
-        if (err) {
-            console.log(`An error occured: ${err}`);
-            response.status(400).send(err);
-        }
-
-
-        else {
-            // Create a request
-            let sqlRequest = new sql.Request(connection);
-
-            // query database
-
-            sqlRequest.query(query, (err: any, sqlResponse: any) => {
-                if (err) {
-                    console.log(`Error while querying the database: ${err}`);
-                    response.status(400).send(err);
-                }
-                else {
-                    response.status(200).send(sqlResponse);
-                }
-            });
-        }
-    });
-}
-
-// GET ALL API
-
-app.get('/api/user', (req: express.Request, response: express.Response) => {
-    let query: string = 'SELECT * FROM [dbo].[user]';
-
-  executeQuery(response, query);
-});
-
-// GET SINGLE API
-
-app.get('/api/user/:user_id', (req: express.Request, response: express.Response) => {
-    let id: number = parseInt(req.params.user_id);
-
-    let query: string = `SELECT * FROM [dbo].[user] WHERE id = \'${id}\'`;
-
-    executeQuery(response, query);
+router.get('/', (req: express.Request, res: express.Response) => {
+    console.log('router');
+    res.json({ message: 'toto' });
 })
 
-// POST API
-app.post('/api/user', (req: express.Request, response: express.Response) => {
-    let id: number = req.body.id;
-    let name: string = req.body.name;
-    let email: string = req.body.email;
-    
-    let query: string = `INSERT INTO [dbo].[user] (id, name, email) VALUES (\'${id}\', \'${name}\', \'${email}')`;
-    executeQuery(response, query);
+// Register routes
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(3000, () => {
+    console.log(`running on port 3000`);
 });
 
-// PUT API
-app.put('/api/user/:id', (req: express.Request, response: express.Response) => {
-    let id: number = parseInt(req.params.id);
-    let newEmail: string = req.body.email;
 
-    let query: string = `UPDATE [dbo].[user] SET email = \'${newEmail}\' WHERE id = \'${id}\'`;
+router.route('/user')
+    // GET ALL API
 
-    executeQuery(response, query);
-});
+    .get((req: express.Request, response: express.Response) => {
+        let query: string = 'SELECT * FROM [dbo].[user]';
 
-// DELETE API
-app.delete('/api/user/:id', (req: express.Request, response: express.Response) => {
-    let id: number = parseInt(req.params.id);
+        db.executeQuery(response, query);
+    })
 
-    let query: string = `DELETE from [dbo].[user] WHERE id = \'${id}\'`;
+    // POST API
+    .post((req: express.Request, response: express.Response) => {
+        let id: number = req.body.id;
+        let name: string = req.body.name;
+        let email: string = req.body.email;
 
-    executeQuery(response, query);
-});
+        let query: string = `INSERT INTO [dbo].[user] (id, name, email) VALUES (\'${id}\', \'${name}\', \'${email}')`;
+        db.executeQuery(response, query);
+    });
+
+
+router.route('/user/:user_id')
+    // GET SINGLE API
+
+    .get((req: express.Request, response: express.Response) => {
+        let id: number = parseInt(req.params.user_id);
+
+        let query: string = `SELECT * FROM [dbo].[user] WHERE id = \'${id}\'`;
+
+        db.executeQuery(response, query);
+    })
+    // PUT API
+    .put((req: express.Request, response: express.Response) => {
+        let id: number = parseInt(req.params.user_id);
+        let newEmail: string = req.body.email;
+
+        let query: string = `UPDATE [dbo].[user] SET email = \'${newEmail}\' WHERE id = \'${id}\'`;
+
+        db.executeQuery(response, query);
+    })
+
+    // DELETE API
+    .delete((req: express.Request, response: express.Response) => {
+        let id: number = parseInt(req.params.user_id);
+
+        let query: string = `DELETE from [dbo].[user] WHERE id = \'${id}\'`;
+
+        db.executeQuery(response, query);
+    });
+
+app.use('/api', router);
