ca0e20bad1b45768abd67d3a5df2e30ddb98353a
Julien DECOTTIGNIES <jdecottignies@absysycborg.com>
2019-05-17 17:24:07 +0200

    add routes folder

---
+++ src/routes/user.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import * as db from '../database';
+
+var router = express.Router();
+
+router.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log('In Router');
+
+    next();
+});
+
+router.get('/',(req: express.Request, response: express.Response) => {
+    let query: string = 'SELECT * FROM [dbo].[user]';
+
+    db.executeQuery(response, query);
+});
+
+// GET SINGLE API
+router.get('/:user_id', (req: express.Request, response: express.Response) => {
+    let id: number = parseInt(req.params.user_id);
+
+    let query: string = `SELECT * FROM [dbo].[user] WHERE id = \'${id}\'`;
+
+    db.executeQuery(response, query);
+});
+
+// POST API
+router.post('', (req: express.Request, response: express.Response) => {
+    let id: number = req.body.id;
+    let name: string = req.body.name;
+    let email: string = req.body.email;
+
+    let query: string = `INSERT INTO [dbo].[user] (id, name, email) VALUES (\'${id}\', \'${name}\', \'${email}')`;
+    db.executeQuery(response, query);
+});
+
+// PUT API
+router.put('/:user_id', (req: express.Request, response: express.Response) => {
+    let id: number = parseInt(req.params.user_id);
+    let newEmail: string = req.body.email;
+
+    let query: string = `UPDATE [dbo].[user] SET email = \'${newEmail}\' WHERE id = \'${id}\'`;
+
+    db.executeQuery(response, query);
+});
+
+// DELETE API
+router.delete('/:user_id', (req: express.Request, response: express.Response) => {
+    let id: number = parseInt(req.params.user_id);
+
+    let query: string = `DELETE from [dbo].[user] WHERE id = \'${id}\'`;
+
+    db.executeQuery(response, query);
+});
+
+module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,7 @@
 import * as bodyParser from "body-parser";
-import * as db from './database';
 import express = require('express');
 
 const app: express.Application = express();
-var router = express.Router();
-
-router.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log('In Router');
-
-    next();
-});
-
-router.get('/', (req: express.Request, res: express.Response) => {
-    console.log('router');
-    res.json({ message: 'toto' });
-})
-
-// Register routes
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,53 +11,5 @@ const server = app.listen(3000, () => {
 });
 
 
-router.route('/user')
-    // GET ALL API
 
-    .get((req: express.Request, response: express.Response) => {
-        let query: string = 'SELECT * FROM [dbo].[user]';
-
-        db.executeQuery(response, query);
-    })
-
-    // POST API
-    .post((req: express.Request, response: express.Response) => {
-        let id: number = req.body.id;
-        let name: string = req.body.name;
-        let email: string = req.body.email;
-
-        let query: string = `INSERT INTO [dbo].[user] (id, name, email) VALUES (\'${id}\', \'${name}\', \'${email}')`;
-        db.executeQuery(response, query);
-    });
-
-
-router.route('/user/:user_id')
-    // GET SINGLE API
-
-    .get((req: express.Request, response: express.Response) => {
-        let id: number = parseInt(req.params.user_id);
-
-        let query: string = `SELECT * FROM [dbo].[user] WHERE id = \'${id}\'`;
-
-        db.executeQuery(response, query);
-    })
-    // PUT API
-    .put((req: express.Request, response: express.Response) => {
-        let id: number = parseInt(req.params.user_id);
-        let newEmail: string = req.body.email;
-
-        let query: string = `UPDATE [dbo].[user] SET email = \'${newEmail}\' WHERE id = \'${id}\'`;
-
-        db.executeQuery(response, query);
-    })
-
-    // DELETE API
-    .delete((req: express.Request, response: express.Response) => {
-        let id: number = parseInt(req.params.user_id);
-
-        let query: string = `DELETE from [dbo].[user] WHERE id = \'${id}\'`;
-
-        db.executeQuery(response, query);
-    });
-
-app.use('/api', router);
+app.use('/api/user', require('./routes/user'));
